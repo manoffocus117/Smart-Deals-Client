3273b361bfc7f4193af416215aab11e9469facb7
MD Saifur Rahman <saifurrahmansaif954@gmail.com>
2026-08-24 16:48:42 +0600

    loader added in root layout

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { use } from "react";
 import Header from "../components/Header";
 import { Outlet } from "react-router";
 import Footer from "../components/Footer";
+import { Auth_context } from "../context/Auth_context";
+import Loading from "../components/Loading";
 
 const Root = () => {
+      const { loading } = use(Auth_context);
+      if (loading) {
+            return <Loading />;
+      }
       return (
             <>
                   <Header />
